6e04fa8a17749d5ed98b15769f8188161a0e5a5c
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2025-07-16 10:40:24 +0200

    firefox: enable weather on new tab page

--- system_files/usr/share/ublue-os/firefox-config/02-botany-global.js
+++ system_files/usr/share/ublue-os/firefox-config/02-botany-global.js
@@ -2,4 +2,11 @@ pref("widget.use-xdg-desktop-portal.file-picker", 1);
 pref("media.webspeech.synth.enabled", false);
 pref("general.autoScroll", true);
 pref("browser.crashReports.unsubmittedCheck.autoSubmit2", true);
+
 pref("browser.newtabpage.activity-stream.default.sites", "https://botany.pl/,https://mail.google.com,https://biblioteka.botany.pl/,https://www.wikipedia.org/,https://rcin.org.pl/dlibra/collectiondescription/428");
+pref("browser.newtabpage.activity-stream.feeds.section.highlights", true); // Recent activity section
+
+pref("browser.newtabpage.activity-stream.system.showWeather", true);
+pref("browser.newtabpage.activity-stream.discoverystream.locale-weather-config", "pl-PL,en-US,en-GB,en-CA"); // doesn't seem to change anything
+pref("browser.newtabpage.activity-stream.discoverystream.region-weather-config", "PL,US,CA"); // doesn't seem to change anything
+//pref("browser.newtabpage.activity-stream.weather.query", 274455); // would change "Krakow" to "Kraków" visually, but I don't want to hardcode the location since it's auto-detected properly
