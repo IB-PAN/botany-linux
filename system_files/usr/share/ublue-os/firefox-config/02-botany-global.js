830d4d9804510159446bd3e0b4e3b653ac1864e1
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2025-07-16 10:28:19 +0200

    firefox: auto-submit crash reports + change default new tab page sites

--- system_files/usr/share/ublue-os/firefox-config/02-botany-global.js
+++ system_files/usr/share/ublue-os/firefox-config/02-botany-global.js
@@ -1,3 +1,5 @@
 pref("widget.use-xdg-desktop-portal.file-picker", 1);
 pref("media.webspeech.synth.enabled", false);
 pref("general.autoScroll", true);
+pref("browser.crashReports.unsubmittedCheck.autoSubmit2", true);
+pref("browser.newtabpage.activity-stream.default.sites", "https://botany.pl/,https://mail.google.com,https://biblioteka.botany.pl/,https://www.wikipedia.org/,https://rcin.org.pl/dlibra/collectiondescription/428");
