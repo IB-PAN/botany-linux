4274b746743e6877c60319f2541e9a0cfd102307
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2025-07-21 11:17:06 +0200

    system-setup.hooks.d: we cannot do this in a child process, or systemd will kill it after parent process exists...

--- system_files/usr/share/ublue-os/system-setup.hooks.d/99-flatpaks-botany.sh
+++ system_files/usr/share/ublue-os/system-setup.hooks.d/99-flatpaks-botany.sh
@@ -8,35 +8,28 @@ version-script flatpaks-botany system 6-$checksum || exit 0
 
 set -x
 
-function run {
-    # removal doesn't need network
-    flatpak list --system --columns=application | grep -Fxf /usr/share/ublue-os/system-flatpaks-remove.list \
-        | xargs --no-run-if-empty flatpak --system --force-remove -y uninstall
+# removal doesn't need network
+flatpak list --system --columns=application | grep -Fxf /usr/share/ublue-os/system-flatpaks-remove.list \
+    | xargs --no-run-if-empty flatpak --system --force-remove -y uninstall
 
-    # needs working internet and DNS
-    while true; do
-        if /usr/share/botany/scripts/is_internet_online.sh; then
-            break
-        fi
-        sleep 1
-    done
-
-    flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
-
-    # Disable Fedora Flatpak remotes
-    for remote in fedora fedora-testing; do
-        if flatpak remote-list | grep -q "$remote"; then
-            flatpak remote-delete "$remote"
-        fi
-    done
-
-    # --reinstall --or-update
-    xargs flatpak --system -y install < /usr/share/ublue-os/system-flatpaks.list
-}
-
-if [ $(systemctl is-active network-online.target) == "active" ]; then
-    run
-else
+# needs working internet and DNS
+while true; do
+    if /usr/share/botany/scripts/is_internet_online.sh; then
+        echo "Network online, continuing..."
+        break
+    fi
     echo "Waiting for network..."
-    run &
-fi
+    sleep 1
+done
+
+flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Disable Fedora Flatpak remotes
+for remote in fedora fedora-testing; do
+    if flatpak remote-list | grep -q "$remote"; then
+        flatpak remote-delete "$remote"
+    fi
+done
+
+# --reinstall --or-update
+xargs flatpak --system -y install < /usr/share/ublue-os/system-flatpaks.list
