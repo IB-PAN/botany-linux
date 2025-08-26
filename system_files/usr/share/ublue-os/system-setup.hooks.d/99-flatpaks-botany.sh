fcefaf7ce7457d69cb102a22e66f28e812b20c65
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2025-08-26 16:15:23 +0200

    fix Fedora Flatpaks removal (hopefully, todo test)

--- system_files/usr/share/ublue-os/system-setup.hooks.d/99-flatpaks-botany.sh
+++ system_files/usr/share/ublue-os/system-setup.hooks.d/99-flatpaks-botany.sh
@@ -4,15 +4,30 @@ checksum=$(sha256sum /usr/share/ublue-os/system-flatpaks*.list | md5sum | awk '{
 
 source /usr/lib/ublue/setup-services/libsetup.sh
 
-version-script flatpaks-botany system 7-$checksum || exit 0
+version-script flatpaks-botany system 8-$checksum || exit 0
 
 set -x
 
-# removal doesn't need network
+# == removal doesn't need network ==
+
 flatpak list --system --columns=application | grep -Fxf /usr/share/ublue-os/system-flatpaks-remove.list \
     | xargs --no-run-if-empty flatpak --system --force-remove -y uninstall
 
-# needs working internet and DNS
+# remove apps from Fedora Flatpak
+flatpak list --system --columns=ref,origin | awk '$2 ~ /^(fedora|fedora-testing)$/ { print $1 }' \
+    | xargs --no-run-if-empty flatpak --system --force-remove -y uninstall
+
+# remove unused refs if any exist
+flatpak --system --force-remove --unused -y uninstall
+
+# disable Fedora Flatpak remotes
+for remote in fedora fedora-testing; do
+    if flatpak remotes --system --columns=name | grep -qFx "$remote"; then
+        flatpak remote-delete --system --force "$remote"
+    fi
+done
+
+# == needs working internet and DNS ==
 while true; do
     if /usr/share/botany/scripts/is_internet_online.sh; then
         echo "Network online, continuing..."
@@ -23,13 +38,6 @@ while true; do
 done
 
 flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# Disable Fedora Flatpak remotes
-for remote in fedora fedora-testing; do
-    if flatpak remote-list | grep -q "$remote"; then
-        flatpak remote-delete "$remote"
-    fi
-done
 
 # --reinstall --or-update
 xargs -n 5 flatpak --system -y install < /usr/share/ublue-os/system-flatpaks.list
