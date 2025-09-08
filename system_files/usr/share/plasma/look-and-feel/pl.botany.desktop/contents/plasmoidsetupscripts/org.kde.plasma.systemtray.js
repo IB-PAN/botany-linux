1116505a5f2dfa70afc42fc22f3bdea17cff117b
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2025-09-08 13:54:16 +0200

    try hiding kopia.io icon

--- system_files/usr/share/plasma/look-and-feel/pl.botany.desktop/contents/plasmoidsetupscripts/org.kde.plasma.systemtray.js
+++ system_files/usr/share/plasma/look-and-feel/pl.botany.desktop/contents/plasmoidsetupscripts/org.kde.plasma.systemtray.js
@@ -3,4 +3,8 @@ if (systemtrayId) {
     const systrayContainer = desktopById(systemtrayId);
     systrayContainer.currentConfigGroup = ["General"];
     systrayContainer.writeConfig("scaleIconsToFit", true);
+    const hiddenItems = systrayContainer.readConfig("hiddenItems").split(",");
+    const hiddenItemsToAdd = ["chrome_status_icon_1"]; // kopia.io
+    hiddenItemsToAdd.forEach(i => { if (!hiddenItems.includes(i)) hiddenItems.push(i); });
+    systrayContainer.writeConfig("hiddenItems", hiddenItems);
 }
