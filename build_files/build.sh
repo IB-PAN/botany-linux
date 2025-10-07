705d36c16e9fe2d5db5949a2de6d13a55b89e442
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2025-10-07 11:01:27 +0200

    minor improvements for Sigillum, will need more testing

--- build_files/build.sh
+++ build_files/build.sh
@@ -185,6 +185,15 @@ chmod +x /tmp/{Sigillum,Sigman}.run
 /tmp/Sigman.run --root "/opt/sigman" --confirm-command --accept-licenses --default-answer --auto-answer OverwriteTargetDirectory=Yes,installationErrorWithCancel=Ignore install
 /tmp/Sigillum.run --confirm-command --accept-licenses --default-answer --auto-answer OverwriteTargetDirectory=Yes,installationErrorWithCancel=Ignore install
 rm /tmp/{Sigillum,Sigman}.run
+desktop-file-edit --set-key=X-GNOME-Autostart-enabled --set-value="false" /etc/xdg/autostart/sigillum_monitor.desktop
+desktop-file-edit --set-key=Hidden --set-value="true" /etc/xdg/autostart/sigillum_monitor.desktop
+# not sure if the below actually does anything, shrug
+# TODO: experiment with those when needed and when having access to a test card
+##ln -s /opt/sigman/sigillum-pkcs11-64.so /usr/lib64/pkcs11/sigillum-pkcs11-64.so
+##echo -e "module: sigillum-pkcs11-64.so\ntrust-policy: yes" > /usr/share/p11-kit/modules/sigillum.module
+##echo -e "\n\nlibrary=/usr/lib64/pkcs11/sigillum-pkcs11-64.so\nname=Sigillum (64 bits)\nNSS=slotParams={0xffffffff=[slotFlags=PublicCerts] 0x0=[slotFlags=PublicCerts] 0x1=[slotFlags=PublicCerts] 0x2=[slotFlags=PublicCerts] 0x3=[slotFlags=PublicCerts] 0x4=[slotFlags=PublicCerts] 0x5=[slotFlags=PublicCerts] 0x6=[slotFlags=PublicCerts] 0x7=[slotFlags=PublicCerts] 0x8=[slotFlags=PublicCerts] 0x9=[slotFlags=PublicCerts] 0xa=[slotFlags=PublicCerts] 0xb=[slotFlags=PublicCerts] 0xc=[slotFlags=PublicCerts] 0xd=[slotFlags=PublicCerts] 0xe=[slotFlags=PublicCerts] 0xf=[slotFlags=PublicCerts]}" >> /etc/pki/nssdb/pkcs11.txt
+# ~/.pki/nssdb/pkcs11.txt | ~/.mozilla/firefox/*.default*/pkcs11.txt | ...
+# modutil -dbdir sql:$HOME/.pki/nssdb/ -add "eToken" -libfile /usr/lib/libeToken.so
 
 #### Example for enabling a System Unit File
 
--- system_files/usr/share/plasma/look-and-feel/pl.botany.desktop/contents/plasmoidsetupscripts/org.kde.plasma.systemtray.js
+++ system_files/usr/share/plasma/look-and-feel/pl.botany.desktop/contents/plasmoidsetupscripts/org.kde.plasma.systemtray.js
@@ -4,7 +4,10 @@ if (systemtrayId) {
     systrayContainer.currentConfigGroup = ["General"];
     systrayContainer.writeConfig("scaleIconsToFit", true);
     const hiddenItems = systrayContainer.readConfig("hiddenItems").split(",");
-    const hiddenItemsToAdd = ["chrome_status_icon_1"]; // kopia.io
+    const hiddenItemsToAdd = [
+        "chrome_status_icon_1", // kopia.io
+        "Sigillum Monitor",
+    ];
     hiddenItemsToAdd.forEach(i => { if (!hiddenItems.includes(i)) hiddenItems.push(i); });
     systrayContainer.writeConfig("hiddenItems", hiddenItems);
 }
