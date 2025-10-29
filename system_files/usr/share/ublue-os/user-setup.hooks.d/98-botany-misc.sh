4166746780df44a63cb95b6a4bd4a092e72043a1
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2025-10-29 10:04:57 +0100

    add Crosswords

--- system_files/usr/share/ublue-os/user-setup.hooks.d/98-botany-misc.sh
+++ system_files/usr/share/ublue-os/user-setup.hooks.d/98-botany-misc.sh
@@ -2,11 +2,16 @@
 
 source /usr/lib/ublue/setup-services/libsetup.sh
 
-version-script botany-misc user 1 || exit 0
+version-script botany-misc user 2 || exit 0
 
 set -x
 
 # Remove Aurora prompt where it was already dropped, we use our global config at /usr/share/botany/starship.toml instead
 if [[ -f "$HOME"/.config/starship.toml && "$(md5sum "$HOME"/.config/starship.toml | awk '{print $1}')" == "b7a2078973f13d2198498053b0ff5043" ]]; then
 	rm -f "$HOME"/.config/starship.toml
+fi
+
+if [[ ! -f "$HOME"/.var/app/org.gnome.Crosswords/config/glib-2.0/settings/keyfile ]]; then
+	mkdir -p "$HOME"/.var/app/org.gnome.Crosswords/config/glib-2.0/settings/
+	echo -e "[org/gnome/Crosswords]\nshown-puzzle-sets=['technopol-daily']" > "$HOME"/.var/app/org.gnome.Crosswords/config/glib-2.0/settings/keyfile
 fi
