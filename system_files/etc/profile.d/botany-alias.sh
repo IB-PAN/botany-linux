1247d8c04258c2e4e0580e84e4d537a8516c8b0e
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2025-12-01 08:38:25 +0100

    botany-alias.sh: add also shorter aliases

--- system_files/etc/profile.d/botany-alias.sh
+++ system_files/etc/profile.d/botany-alias.sh
@@ -1,4 +1,9 @@
 alias botany_sudo='/usr/sbin/sudo -u botany_adm /usr/sbin/sudo'
 alias botany_su='/usr/sbin/sudo -u botany_adm /usr/sbin/sudo /usr/sbin/su -'
 alias botany_adm='/usr/sbin/sudo -u botany_adm -i ;'
+
+alias bsudo=botany_sudo
+alias bsu=botany_su
+alias badm=botany_adm
+
 alias msedit='/usr/bin/msedit'
