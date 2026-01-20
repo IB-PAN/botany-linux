c948fe69232af54260d1066fa61cddaab726b763
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2026-01-20 09:38:07 +0100

    duperemove: further improvements, delete old inflated database
    Frankly the database would shrink itself after the next run, but it'd still take a lot of time for re-scanning and skipping of the entries. Plus we'll probably switch to bees, so the database being gone at that point will be convenient for that in the probable next commit.

--- system_files/usr/share/ublue-os/system-setup.hooks.d/95-botany-misc.sh
+++ system_files/usr/share/ublue-os/system-setup.hooks.d/95-botany-misc.sh
@@ -2,7 +2,7 @@
 
 source /usr/lib/ublue/setup-services/libsetup.sh
 
-version-script botany-misc system 3 || exit 0
+version-script botany-misc system 4 || exit 0
 
 set -x
 
@@ -23,3 +23,8 @@ rm -rf /var/home/linuxbrew /var/lib/homebrew /var/cache/homebrew \
 	/etc/.linuxbrew /etc/profile.d/brew{,-bash-completion}.sh /etc/security/limits.d/30-brew-limits.conf \
 	/etc/systemd/system/{default,multi-user}.target.wants/brew-setup.service \
     /etc/systemd/system/timers.target.wants/brew-{update,upgrade}.timer
+
+# Remove duperemove database if it was made before the period where it was overly inflated
+if [ -d /var/lib/duperemove ]; then
+	find /var/lib/duperemove -name '-var-home.hashfile*' -exec bash -c 'LC_ALL=C stat "$0" | grep -qF "Birth: 2025-" && rm -f "$0"' {} \;
+fi
