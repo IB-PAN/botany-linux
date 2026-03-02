815315781bfd7820da8d4a8906202f30b1537bab
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2026-03-02 13:42:24 +0100

    R: try relocating CRAN packages this way

--- build_files/50-rstudio.sh
+++ build_files/50-rstudio.sh
@@ -11,14 +11,15 @@ copr_install_isolated "iucar/rstudio" "rstudio-desktop" "rstudio-server"
 jq --arg desktopName "rstudio.desktop" '.["desktopName"] = ($desktopName)' /usr/libexec/rstudio/package.json | sponge /usr/libexec/rstudio/package.json
 
 # Extra CRAN packages using cran2copr repo
-# - languageserver is needed for VS Code extension
-mkdir -p /usr/local/lib/R/library/
-copr_install_isolated "iucar/cran" "R-CRAN-languageserver" \
+pdnf copr enable "iucar/cran"
+mkdir -p /tmp/crans
+pdnf download --resolve --destdir=/tmp/crans --from-repo="copr:copr.fedorainfracloud.org:iucar:cran" \
+    "R-CRAN-languageserver" \
     "R-CRAN-rmarkdown" "R-CRAN-rgbif" "R-CRAN-spocc" "R-CRAN-maps" "R-CRAN-mapproj" "R-CRAN-mapdata" "R-CRAN-dismo" "R-CRAN-raster" "R-CRAN-plotly" \
-    "R-CRAN-RColorBrewer" "R-CRAN-tidyverse" "R-CRAN-readxl"
-
-# R-CRAN packages install to wrong dir...
-rsync -avh --remove-source-files /usr/local/lib/R/library/ /usr/lib64/R/library/
-rm -rf /usr/local/lib/R/library/
+    "R-CRAN-RColorBrewer" "R-CRAN-tidyverse" "R-CRAN-readxl" "R-CRAN-leafpop" "R-CRAN-geodata"
+find /tmp/crans -name '*.src.rpm' -type f -delete
+rpm --install --replacefiles --badreloc --relocate /usr/local/lib/R/library/=/usr/lib64/R/library/ /tmp/crans/*.rpm
+pdnf copr disable "iucar/cran"
+rm -rf /tmp/crans
 
 #copr_install_isolated "@copr/PyPI" "python-radian"
