12b299abdcd9f943aab13df5adad604016cf7bb1
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2026-05-12 13:42:57 +0200

    fix KopiaUI icon hiding after the update

--- system_files/usr/share/plasma/look-and-feel/pl.botany.desktop/contents/plasmoidsetupscripts/org.kde.plasma.systemtray.js
+++ system_files/usr/share/plasma/look-and-feel/pl.botany.desktop/contents/plasmoidsetupscripts/org.kde.plasma.systemtray.js
@@ -5,7 +5,7 @@ if (systemtrayId) {
     systrayContainer.writeConfig("scaleIconsToFit", true);
     const hiddenItems = systrayContainer.readConfig("hiddenItems").split(",");
     const hiddenItemsToAdd = [
-        "chrome_status_icon_1", // kopia.io
+        "KopiaUI_status_icon_1",
         "Sigillum Monitor",
     ];
     hiddenItemsToAdd.forEach(i => { if (!hiddenItems.includes(i)) hiddenItems.push(i); });
