0a4a64bcacdacc9891870a28d5752eb208d1b90d
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2026-05-25 10:09:45 +0200

    50-packages: add kcharselect

--- build_files/50-packages.sh
+++ build_files/50-packages.sh
@@ -8,7 +8,7 @@ pdnf install screen signon-kwallet-extension signon-ui tecla gphoto2 v4l-utils x
     krusader krename kompare md5sum lhasa unrar xz-lzma-compat \
     pam-u2f pamu2fcfg fido2-tools yubikey-manager fprintd fprintd-pam cups-pk-helper \
     gnome-commander doublecmd-qt6 \
-    kcalc gwenview okular kweather krecorder haruna kolourpaint kcolorchooser qdirstat kdiskmark filelight cpu-x audacity diffpdf \
+    kcalc gwenview okular kweather krecorder haruna kolourpaint kcolorchooser kcharselect qdirstat kdiskmark filelight cpu-x audacity diffpdf \
     sbsigntools zram-generator stress stress-ng memtester monitor-edid edid-decode drm_info rocm-smi igt-gpu-tools \
     ripgrep msedit \
     wine q4wine wine-dxvk wine-mono winetricks \
