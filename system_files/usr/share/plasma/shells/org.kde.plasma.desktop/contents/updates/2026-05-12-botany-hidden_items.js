aefe21438d650050050951d4700c7b1ad5f3eaf9
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2026-06-22 10:09:29 +0200

    plasma: fix location of update script that updates hiddenItems

---
+++ system_files/usr/share/plasma/shells/org.kde.plasma.desktop/contents/updates/2026-05-12-botany-hidden_items.js
@@ -0,0 +1,18 @@
+for (const panel of panels()) {
+    for (const widget of panel.widgets("org.kde.plasma.systemtray")) {
+        widget.currentConfigGroup = ["General"];
+        print(widget.type + "\n");
+        print(widget.readConfig("hiddenItems") + "\n");
+        const hiddenItems = widget.readConfig("hiddenItems").split(",");
+        const hiddenItemsToAdd = [
+            "KopiaUI_status_icon_1",
+            "Sigillum Monitor",
+        ];
+        for (const item of hiddenItemsToAdd) {
+            if (!hiddenItems.includes(item))
+                hiddenItems.push(item);
+        }
+        widget.writeConfig("hiddenItems", hiddenItems);
+        widget.reloadConfig();
+    }
+}
