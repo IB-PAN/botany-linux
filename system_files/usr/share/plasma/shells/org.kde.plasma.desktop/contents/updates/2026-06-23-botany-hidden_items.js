990d01a60e9f06714e7f4aa85f382800b36c07e8
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2026-06-23 10:36:16 +0200

    plasma updates: hide PikaBackup background icon

--- system_files/usr/share/plasma/shells/org.kde.plasma.desktop/contents/updates/2026-06-23-botany-hidden_items.js
+++ system_files/usr/share/plasma/shells/org.kde.plasma.desktop/contents/updates/2026-06-23-botany-hidden_items.js
@@ -1,12 +1,13 @@
 for (const panel of panels()) {
     for (const widget of panel.widgets("org.kde.plasma.systemtray")) {
         widget.currentConfigGroup = ["General"];
-        print(widget.type + "\n");
-        print(widget.readConfig("hiddenItems") + "\n");
+        //print(widget.type + "\n");
+        //print(widget.readConfig("hiddenItems") + "\n");
         const hiddenItems = widget.readConfig("hiddenItems").split(",");
         const hiddenItemsToAdd = [
             "KopiaUI_status_icon_1",
             "Sigillum Monitor",
+            "org.gnome.World.PikaBackup",
         ];
         for (const item of hiddenItemsToAdd) {
             if (!hiddenItems.includes(item))
