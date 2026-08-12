36ad2af62ca0b9caa799f93e1c15f6b93fd6182b
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2026-08-12 12:06:34 +0200

    50-packages: add ntfs2btrfs

--- build_files/50-packages.sh
+++ build_files/50-packages.sh
@@ -13,7 +13,7 @@ pdnf install screen signon-kwallet-extension signon-ui tecla gphoto2 v4l-utils x
     ripgrep msedit \
     wine q4wine wine-dxvk wine-mono winetricks \
     samba samba-tools \
-    gparted gsmartcontrol btrfs-assistant btrfsmaintenance snapper xfsprogs-xfs_scrub duperemove fdupes ncdu sysstat \
+    gparted gsmartcontrol btrfs-assistant btrfsmaintenance snapper xfsprogs-xfs_scrub duperemove fdupes ncdu sysstat ntfs2btrfs \
     dialog freerdp git iproute libnotify nmap-ncat iperf3 podman-compose \
     tesseract-langpack-pol tesseract-langpack-eng \
     sane-backends sane-airscan \
